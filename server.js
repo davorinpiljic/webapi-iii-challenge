48f12267c53e304f4c6360cc9c6e24e954470f8a
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-03 23:22:42 -0600

    added endpoints to retrieve list of posts for user and store a new post for a user

--- server.js
+++ server.js
@@ -1,109 +1,123 @@
-const express = require('express');
-const userDb = require('./users/userDb.js')
-const postDb = require('./posts/postDb.js')
-const cors = require('cors')
+const express = require("express");
+const userDb = require("./users/userDb.js");
+const postDb = require("./posts/postDb.js");
+const cors = require("cors");
 
 const server = express();
 server.use(express.json());
-server.use(cors())
+server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Good morning World! We do middleware today!</h2>`)
-});
-
-//custom middleware
 function logger(request, response, next) {
-  console.log(`Request method: ${request.method}, Request url: ${request.url}, Request time: ${new Date()}`)
-  next()
+  console.log(
+    `Request method: ${request.method}, Request url: ${
+      request.url
+    }, Request time: ${new Date()}`
+  );
+  next();
 }
 function validateUserId(request, response, next) {
-  const { id } = request.params
-  userDb.getById(id)
-  .then(user => {
-  if (user) {
-      request.user = user
-      next()
-  }
-  else {
-      response.status(400).json({message: "invalid user id"})
-  }
-}) 
+  const { id } = request.params;
+  userDb.getById(id).then(user => {
+    if (user) {
+      next();
+    } else {
+      response.status(400).json({ message: "invalid user id" });
+      next();
+    }
+  });
 }
-function validateUser(request, response, next) {
-  const requestBody = request.body
-  if (!requestBody) {
-      response.status(400).json({ message: "missing user data" })
-  }
-  else if (!requestBody.name) {
-      response.status(400).json({ message: "missing required text field"})
-  }
-}
+
 function validatePost(request, response, next) {
-  const requestBody = request.body
-  if (!requestBody.name) {
-    response.status(400).json({ message: "missing data"})
+  const requestBody = request.body;
+  console.log(requestBody);
+  if (Object.keys(requestBody).length === 0) {
+    response.status(400).json({ message: "missing post data" });
+    next();
   }
-
-  next()
+  if (!requestBody.text) {
+    response.status(400).json({ message: "missing required text field" });
+    next();
+  } else {
+    next();
+  }
 }
 
-server.use(logger)
+function validateUser(request, response, next) {
+  const requestBody = request.body;
+  console.log(requestBody);
+  if (Object.keys(requestBody).length === 0) {
+    response.status(400).json({ message: "missing user data" });
+    next();
+  }
+  if (!requestBody.name) {
+    response.status(400).json({ message: "missing required name field" });
+    next();
+  } else {
+    next();
+  }
+}
+
+server.use(logger);
+
+//endpoints
+server.get("/", (req, res) => {
+  res.send(`<h2>Good morning World! We do middleware today!</h2>`);
+});
+
 //crud users
-server.post('/users', (request, response) => {
-  const requestBody = request.body
-  userDb.insert(requestBody)
-  .then(user => {
-    if(user) {
-      response.status(201).json(user)
+server.post("/users", validateUser, (request, response) => {
+  const requestBody = request.body;
+  userDb.insert(requestBody).then(user => {
+    if (user) {
+      response.status(201).json(user);
     }
-  })
-})
-server.get('/users', (request, response) => {
-  const requestBody = request.body
-  userDb.get()
-  .then(users => {
-    if(users) {
-      response.status(200).json(users)
+  });
+});
+server.get("/users", (request, response) => {
+  userDb.get().then(users => {
+    if (users) {
+      response.status(200).json(users);
     }
-  })
-})
-server.put('/users/:id', (request, response) => {
-  const updatedObject = request.body
-  const { id } = request.params
-  userDb.update(id, updatedObject)
-  .then(users => {
-    if(users) {
-      response.status(200).json(users)
+  });
+});
+server.put("/users/:id", (request, response) => {
+  const updatedObject = request.body;
+  const { id } = request.params;
+  userDb.update(id, updatedObject).then(users => {
+    if (users) {
+      response.status(200).json(users);
     }
-  })
-})
-server.delete('/users/:id', (request, response) => {
-  const { id } = request.params
-  userDb.remove(id) 
-  .then(users => {
-    response.status(200).json(users)
-  })
-})
+  });
+});
+server.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+  userDb.remove(id).then(users => {
+    response.status(200).json(users);
+  });
+});
+server.get("/user/:id/posts", validateUserId, (request, response) => {
+  userDb.getUserPosts(request.params.id).then(posts => {
+    if (posts) {
+      response.status(200).json(posts);
+    }
+  });
+});
+server.post("/user/:id", validateUserId, validatePost, (request, response) => {
+  postDb.insert(request.body).then(post => {
+    if (post) {
+      response.status(201).json(post);
+    }
+  });
+});
+//crud posts
+server.get("/posts", (request, response) => {
+  postDb.get().then(posts => {
+    if (posts) {
+      response.status(200).json(posts);
+    }
+  });
+});
 
-//
-server.get('/user/:id/posts', validateUserId, (request, response) => {
-  postDb.getById(request.user.id)
-  .then(posts => {
-    if(posts) {
-      response.status(200).json(posts)
-    }
-  })
-})
-
-server.post('/user/:id', validatePost, (request, response) => {
-  postDb.insert(request.body)
-  .then(post => {
-    if(post) {
-      response.status(201).json(post)
-    }
-  })    
-})
-
-server.listen(7000, () => console.log('running'))
+server.listen(7000, () => console.log("API Running on 7000, SUCCESS"));
 
 module.exports = server;
